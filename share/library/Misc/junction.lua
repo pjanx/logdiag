6a8238846c53440dad44f2f3678ff45360b29927
Přemysl Janouch <p.janouch@gmail.com>
2011-02-15 18:04:31 +0100

    Add a symbol for junctions.

---
+++ share/library/Misc/junction.lua
@@ -0,0 +1,24 @@
+-- Symbol name
+local names =
+{
+	en = "Junction",
+	cs = "Spoj"
+}
+
+-- Render area in base units (X1, Y1, X2, Y2)
+local area = {-0.5, -0.5, 0.5, 0.5}
+
+-- Terminal points
+local terminals = {}
+
+-- Rendering
+local render = function (cr)
+	-- The disk
+	cr.arc (0, 0, 0.3, 0, math.pi * 2)
+	cr.fill ()
+end
+
+-- Register the symbol
+logdiag.register ("Junction", names, area, terminals, render)
+
+
